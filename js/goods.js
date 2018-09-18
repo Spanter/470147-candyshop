f0346e68e18c0161947e523dce0b542b9dd557b9
Maksim Ivanchenko <35325543+Spanter@users.noreply.github.com>
2018-09-19 00:49:41 +1000

    Не проверять
    Подготовка пееменных.

---
+++ js/goods.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var goodData = {
+  NAME: ['Чесночные сливки',
+    'Огуречный педант',
+    'Молочная хрюша',
+    'Грибной шейк',
+    'Баклажановое безумие',
+    'Паприколу итальяно',
+    'Нинзя-удар васаби',
+    'Хитрый баклажан',
+    'Горчичный вызов',
+    'Кедровая липучка',
+    'Корманный портвейн',
+    'Чилийский задира',
+    'Беконовый взрыв',
+    'Арахис vs виноград',
+    'Сельдерейная душа',
+    'Початок в бутылке',
+    'Чернющий мистер чеснок',
+    'Раша федераша',
+    'Кислая мина',
+    'Кукурузное утро',
+    'Икорный фуршет',
+    'Новогоднее настроение',
+    'С пивком потянет',
+    'Мисс креветка',
+    'Бесконечный взрыв',
+    'Невинные винные',
+    'Бельгийское пенное',
+    'Острый язычок'],
+
+  PICTURE: ['img/cards/gum-cedar.jpg',
+    'img/cards/gum-chile.jpg',
+    'img/cards/gum-eggplant.jpg',
+    'img/cards/gum-mustard.jpg',
+    'img/cards/gum-portwine.jpg',
+    'img/cards/gum-wasabi.jpg',
+    'img/cards/ice-cucumber.jpg',
+    'img/cards/ice-eggplant.jpg',
+    'img/cards/ice-garlic.jpg',
+    'img/cards/ice-italian.jpg',
+    'img/cards/ice-mushroom.jpg',
+    'img/cards/ice-pig.jpg',
+    'img/cards/marmalade-beer.jpg',
+    'img/cards/marmalade-caviar.jpg',
+    'img/cards/marmalade-corn.jpg',
+    'img/cards/marmalade-new-year.jpg',
+    'img/cards/marmalade-sour.jpg',
+    'img/cards/marshmallow-bacon.jpg',
+    'img/cards/marshmallow-beer.jpg',
+    'img/cards/marshmallow-shrimp.jpg',
+    'img/cards/marshmallow-spicy.jpg',
+    'img/cards/marshmallow-wine.jpg',
+    'img/cards/soda-bacon.jpg',
+    'img/cards/soda-celery.jpg',
+    'img/cards/soda-cob.jpg',
+    'img/cards/soda-garlic.jpg',
+    'img/cards/soda-peanut-grapes.jpg',
+    'img/cards/soda-russian.jpg'],
+
+  AMOUNT: {
+    min: 0,
+    max: 20
+  },
+
+  PRICE: {
+    min: 100,
+    max: 1500
+  },
+
+  WEIGHT: {
+    min: 30,
+    max: 300
+  },
+
+  RATING_VALUE: {
+    min: 1,
+    max: 5
+  },
+  RATING_NUMBERS: {
+    min: 10,
+    max: 900
+  },
+
+  NUTRITIONFACTS_SUGAR: [true, false],
+
+  NUTRITIONFACTS_ENERGY: {
+    min: 70,
+    max: 500
+  },
+
+  NUTRITIONFACTS_CONTENT: ['молоко',
+    'сливки',
+    'вода',
+    'пищевой краситель',
+    'патока',
+    'ароматизатор бекона',
+    'ароматизатор свинца',
+    'ароматизатор дуба, идентичный натуральному',
+    'ароматизатор картофеля',
+    'лимонная кислота',
+    'загуститель',
+    'эмульгатор',
+    'консервант: сорбат калия',
+    'посолочная смесь: соль, нитрит натрия',
+    'ксилит',
+    'карбамид',
+    'вилларибо',
+    'виллабаджо'],
+
+  NUMBER: 26,
+
+  NUMBER_IN_CARD: 3
+};
+
